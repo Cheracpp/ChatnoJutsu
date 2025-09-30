9bed9b2761890d7b6cbff91ab83574f0c568b70b
[cheracpp] <aymanecheracher@gmail.com>
2025-09-30 22:19:14 +0100

    fix(auth): redirect unauthenticated users to a default login page

--- src/main/java/com/aymane/chatnojutsu/config/SecurityConfig.java
+++ src/main/java/com/aymane/chatnojutsu/config/SecurityConfig.java
@@ -17,6 +17,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import static jakarta.servlet.DispatcherType.ERROR;
@@ -26,59 +27,66 @@ import static jakarta.servlet.DispatcherType.FORWARD;
 @EnableWebSecurity
 public class SecurityConfig {
 
-    private final JwtService jwtService;
-    private final UserDetailsService userDetailsService;
-    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
-    @Autowired
-    public SecurityConfig(JwtService jwtService,
-                          UserDetailsService userDetailsService,
-                          JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint) {
-        this.jwtService = jwtService;
-        this.userDetailsService = userDetailsService;
-        this.jwtAuthenticationEntryPoint = jwtAuthenticationEntryPoint;
-    }
-    @Bean
-    public AuthenticationManager authenticationManager(
-            UserDetailsService userDetailsService,
-            PasswordEncoder passwordEncoder) {
-        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
-        authenticationProvider.setUserDetailsService(userDetailsService);
-        authenticationProvider.setPasswordEncoder(passwordEncoder);
+  private final JwtService jwtService;
+  private final UserDetailsService userDetailsService;
+  private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
 
-        return new ProviderManager(authenticationProvider);
-    }
-    @Bean
-    public PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder();
-    }
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        JwtFilter jwtFilter = new JwtFilter(jwtService, userDetailsService);
-        httpSecurity
-                .headers(headers ->
-                        headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::deny))
-                .cors(AbstractHttpConfigurer::disable)
-                .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(authorizeRequests ->
-                        authorizeRequests
-                                .requestMatchers(HttpMethod.POST,"/users","/auth/login").permitAll()
-                                .dispatcherTypeMatchers(FORWARD,ERROR).permitAll()
-                                .requestMatchers("/login").permitAll()
-                                .requestMatchers("/register").permitAll()
-                                .requestMatchers("/resources/**").permitAll()
-                                .requestMatchers("/static/**").permitAll()
-                                .requestMatchers("/templates/*").permitAll()
-                                .requestMatchers("/css/*").permitAll()
-                                .requestMatchers("/images/*").permitAll()
-                                .requestMatchers("/javascript/*").permitAll()
-                                .anyRequest().authenticated()
-                )
-                .sessionManagement(session ->
-                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                )
-                .exceptionHandling( (exceptionHandlingConfigurer)  -> exceptionHandlingConfigurer.authenticationEntryPoint(jwtAuthenticationEntryPoint))
-                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+  @Autowired
+  public SecurityConfig(JwtService jwtService,
+      UserDetailsService userDetailsService,
+      JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint) {
+    this.jwtService = jwtService;
+    this.userDetailsService = userDetailsService;
+    this.jwtAuthenticationEntryPoint = jwtAuthenticationEntryPoint;
+  }
 
-        return httpSecurity.build();
-    }
+  @Bean
+  public AuthenticationManager authenticationManager(
+      UserDetailsService userDetailsService,
+      PasswordEncoder passwordEncoder) {
+    DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+    authenticationProvider.setUserDetailsService(userDetailsService);
+    authenticationProvider.setPasswordEncoder(passwordEncoder);
+
+    return new ProviderManager(authenticationProvider);
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+    JwtFilter jwtFilter = new JwtFilter(jwtService, userDetailsService);
+    httpSecurity
+        .headers(headers ->
+            headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::deny))
+        .cors(AbstractHttpConfigurer::disable)
+        .csrf(AbstractHttpConfigurer::disable)
+        .formLogin(AbstractHttpConfigurer::disable)
+        .authorizeHttpRequests(authorizeRequests ->
+            authorizeRequests
+                .requestMatchers(HttpMethod.POST, "/users", "/auth/login").permitAll()
+                .dispatcherTypeMatchers(FORWARD, ERROR).permitAll()
+                .requestMatchers("/login").permitAll()
+                .requestMatchers("/register").permitAll()
+                .requestMatchers("/resources/**").permitAll()
+                .requestMatchers("/static/**").permitAll()
+                .requestMatchers("/templates/*").permitAll()
+                .requestMatchers("/css/*").permitAll()
+                .requestMatchers("/images/*").permitAll()
+                .requestMatchers("/javascript/*").permitAll()
+                .anyRequest().authenticated()
+        )
+        .sessionManagement(session ->
+            session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        )
+        .exceptionHandling(
+            ex -> ex.authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/login"))
+        )
+        .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+
+    return httpSecurity.build();
+  }
 }
--- src/main/resources/static/javascript/login.js
+++ src/main/resources/static/javascript/login.js
@@ -1,22 +1,22 @@
 $(document).ready(function () {
     $("#myForm").on("submit", function (event) {
         event.preventDefault();
-        var formData = {};
+        let formData = {};
         $(this).find("input").each(function () {
             formData[this.name] = $(this).val();
         });
 
         $.ajax({
             type: "POST",
-            url: "http://localhost:8080/auth/login",
+            url: "/auth/login",
             contentType: "application/json",
             data: JSON.stringify(formData),
             success: function (response, status, xhr) {
                 const username = document.getElementById('username').value;
                 localStorage.setItem('username', username);
-                var jwt = xhr.getResponseHeader('Set-Cookie');
+                let jwt = xhr.getResponseHeader('Set-Cookie');
                 console.log("user logged in, jwt: ", jwt);
-                window.location.href = "http://localhost:8080/home";
+                window.location.href = "/home";
             },
             error: function (xhr, status, error) {
                 console.error("Request failed: ", status, error);
