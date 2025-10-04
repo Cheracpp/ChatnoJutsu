e5f472781d264262b4e43ed4b4d51837c10758dd
[cheracpp] <aymanecheracher@gmail.com>
2025-10-04 16:41:33 +0100

    refactor(model): update message and room models
    This refactor improves the overall flow of the application by:
    - Adding support for group messaging rooms.
    - Decoupling message persistence from room ID retrieval.
    - Delegating date field management to Spring.

--- src/main/java/com/aymane/chatnojutsu/model/Message.java
+++ src/main/java/com/aymane/chatnojutsu/model/Message.java
@@ -1,26 +1,27 @@
 package com.aymane.chatnojutsu.model;
 
+import java.time.Instant;
 import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.CompoundIndex;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.util.Date;
 
 @Builder
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-@Document(collection = "message")
-@CompoundIndex(name = "room_created_idx", def = "{'roomId': 1, 'createdAt': 1}")
+@Document(collection = "messages")
+@CompoundIndex(name = "room_created_idx", def = "{'roomId': 1, 'timestamp': 1}")
 public class Message {
     @Id
     private String messageId;
     private String roomId;
-    private String messageFrom;
-    private String messageTo;
+    private String senderId;
     private String content;
-    private Date createdAt;
+    @CreatedDate
+    private Instant timestamp;
 }
 
--- src/main/java/com/aymane/chatnojutsu/model/Room.java
+++ src/main/java/com/aymane/chatnojutsu/model/Room.java
@@ -2,8 +2,8 @@ package com.aymane.chatnojutsu.model;
 
 import jakarta.persistence.Id;
 import lombok.*;
-import org.springframework.data.mongodb.core.index.CompoundIndex;
-import org.springframework.data.mongodb.core.index.CompoundIndexes;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.Date;
@@ -14,15 +14,13 @@ import java.util.Date;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-@CompoundIndexes({
-    @CompoundIndex(name = "participants_idx", def = "{'messageFrom': 1, 'messageTo': 1}"),
-    @CompoundIndex(name = "from_lastSent_idx", def = "{'messageFrom': 1, 'lastMessageSentAt': -1}"),
-    @CompoundIndex(name = "to_lastSent_idx", def = "{'messageTo': 1, 'lastMessageSentAt': -1}")
-})
 public class Room {
     @Id
-    private String id;
+    private String roomId;
+    private String[] participants;
+    private String type;
+    @LastModifiedDate
     private Date lastMessageSentAt;
-    private String messageFrom;
-    private String messageTo;
+    @CreatedDate
+    private Date createdAt;
 }
